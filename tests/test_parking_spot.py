c60a84a77906310df2d5eb7e12aafeda2d5fde3a
Shenbakesh Kishore <shenbakesh.manoharan@toppr.com>
2020-03-31 11:09:53 +0530

    test parking-spot type(isinstance)

--- tests/test_parking_spot.py
+++ tests/test_parking_spot.py
@@ -1,5 +1,6 @@
 import unittest
 
+from parking_lot.constants import ParkingSpotType
 from parking_lot.parking_spot import ParkingSpot, FourWheelerSpot
 
 
@@ -9,3 +10,10 @@ class TestParkingSpot(unittest.TestCase):
             parking_spot = FourWheelerSpot()
             self.assertIsInstance(parking_spot, ParkingSpot)
             self.assertEqual(i, parking_spot.id_)
+
+    def test_parking_spot_type(self):
+        for i in range(1, 11):
+            parking_spot = FourWheelerSpot()
+            self.assertIs(
+                parking_spot.parking_spot_type, 
+                ParkingSpotType.FOUR_WHEELER)
