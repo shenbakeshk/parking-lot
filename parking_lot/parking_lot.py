373f6cc17c6627855bca636294d55be1ed66e9f9
Shenbakesh Kishore <shenbakesh.manoharan@toppr.com>
2020-04-03 14:57:00 +0530

    select next available parking-spot for incoming vehicle based on vehicle-type

--- parking_lot/parking_lot.py
+++ parking_lot/parking_lot.py
@@ -2,6 +2,7 @@ import itertools
 from typing import List
 
 from parking_lot.constants import VehicleType
+from parking_lot.parking_spot import ParkingSpot
 
 
 class ParkingLot:
@@ -62,3 +63,15 @@ class ParkingLot:
                 < self._max_four_wheeler_spots
         else:
             raise Exception("Invalid vehicle type request")
+
+    def _select_next_available_parking_spot(
+        self, vehicle_type: VehicleType
+    ) -> ParkingSpot:
+        """
+        Select and return next available parking-spot based on vehicle type. 
+        Return parking-spot.
+        """
+        if vehicle_type is VehicleType.CAR:
+            parking_spot = self._four_wheeler_spots[
+                self._next_four_wheeler_spot]
+        return parking_spot
