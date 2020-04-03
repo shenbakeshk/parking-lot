e9dd551ea8d65305fc05c9aa871eb9b568017094
Shenbakesh Kishore <shenbakesh.manoharan@toppr.com>
2020-04-03 13:49:46 +0530

    initialize next parking-spot id(itertools.count) from prev obj

--- tests/test_parking_spot.py
+++ tests/test_parking_spot.py
@@ -6,7 +6,10 @@ from parking_lot.parking_spot import ParkingSpot, FourWheelerSpot
 
 class TestParkingSpot(unittest.TestCase):
     def test_parking_spot(self):
-        for i in range(1, 11):
+        parking_spot = FourWheelerSpot()
+        start = parking_spot.id_ + 1
+        end = start + 100
+        for i in range(start, end):
             parking_spot = FourWheelerSpot()
             self.assertIsInstance(parking_spot, ParkingSpot)
             self.assertEqual(i, parking_spot.id_)
